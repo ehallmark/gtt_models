dfb794545e8249dd455a2d78ba2239cf380e451c
ehallmark <ehallmark@gttgrp.com>
2018-05-26 13:14:22 -0700

    c

--- src/java_compatibility/MakeEncoderJavaCompatible.py
+++ src/java_compatibility/MakeEncoderJavaCompatible.py
@@ -33,21 +33,63 @@ def load_cpc2vec_index_maps():
     return Word2VecModel.build_dictionaries()
 
 
-def encode_text(model, word_to_idx, text):
-    words = re.sub('[^a-z ]', ' ', text.lower()).split()
-    words = [word_to_idx[word] for word in words if word in word_to_idx]
-    if len(words) > 128:
-        words = words[0:128]
-    elif len(words) < 128:
-        words = ([0] * (128 - len(words))) + words
-    x = np.array(words).reshape((1, 128, 1))
-    return model.predict(x).flatten()
+def encode_text(model, word_to_idx, texts):
+    all_vectors = []
+    all_words = []
+    invalid = set([])
+    for i in range(len(texts)):
+        text = texts[i]
+        if text is not None:
+            words = re.sub('[^a-z ]', ' ', text.lower()).split()
+            words = [word_to_idx[word] for word in words if word in word_to_idx]
+        else:
+            words = []
+        if len(words) < 3:
+            invalid.add(i)
+            words = [0] * 128
+        else:
+            if len(words) > 128:
+                words = words[0:128]
+            elif len(words) < 128:
+                words = ([0] * (128 - len(words))) + words
+        all_words.append(words)
+    x = np.array(all_words).reshape((len(all_words), 128, 1))
+    y = model.predict(x)
+    for i in range(y.shape[0]):
+        if i in invalid:
+            all_vectors.append(None)
+        else:
+            all_vectors.append(y[i, :].flatten())
+    return all_vectors
 
 
-def encode_cpcs(model, cpc_to_idx, cpcs):
-    cpcs = [cpc_to_idx[cpc] for cpc in cpcs if cpc in cpc_to_idx]
-    x = np.array(cpcs).reshape((len(cpcs), 1, 1))
-    return model.predict(x).sum(0)
+def encode_cpcs(model, cpc_to_idx, cpc_trees):
+    all_cpcs = []
+    intervals = []
+    invalid = set()
+    for i in range(len(cpc_trees)):
+        cpcs = cpc_trees[i]
+        if cpcs is None:
+            invalid.add(i)
+            intervals.append(0)
+        else:
+            cpcs = [cpc_to_idx[cpc] for cpc in cpcs if cpc in cpc_to_idx]
+            intervals.append(len(cpcs))
+            for cpc in cpcs:
+                all_cpcs.append(cpc)
+
+    x = np.array(all_cpcs).reshape((len(all_cpcs), 1, 1))
+    y = model.predict(x)
+    vecs = []
+    idx = 0
+    for i in range(len(intervals)):
+        interval = intervals[i]
+        if i in invalid:
+            vecs.append(None)
+        else:
+            vecs.append(y[idx:idx+interval, :].sum(0))
+        idx += interval
+    return vecs
 
 
 model_file_32 = '/home/ehallmark/data/python/w2v_cpc_rnn_model_keras32.h5'
@@ -61,11 +103,12 @@ if __name__ == '__main__':
     seed_cursor = conn.cursor("stream")
     seed_cursor.itersize = 100
     seed_cursor.execute("""select p.family_id, p.abstract, tree from big_query_patent_english_abstract as p
-       join big_query_cpc_tree as c on (p.publication_number_full=c.publication_number_full)""")
+       left outer join big_query_cpc_tree as c on (p.publication_number_full=c.publication_number_full)""")
 
     ingest_cursor = conn2.cursor()
-    ingest_sql = """insert into big_query_embedding_by_fam (family_id,enc) values (%s,%s) 
-      on conflict (family_id) do update set enc=excluded.enc"""
+    ingest_sql_prefix = """insert into big_query_embedding_by_fam (family_id,enc) values """
+    ingest_sql_suffix = """ on conflict (family_id) do update set enc=excluded.enc"""
+
     print("Querying...")
 
     word_idx_map, idx_word_map = load_word2vec_index_maps()
@@ -100,33 +143,47 @@ if __name__ == '__main__':
     # load model
     cnt = 0
     not_found = 0
+    text_batch = []
+    cpc_batch = []
+    id_batch = []
+    batch_size = 512
     for record in seed_cursor:
         id = record[0]
         text = record[1]
         cpc_tree = record[2]
 
-        cpc_encoding = encode_cpcs(cpc_model, cpc_idx_map, cpc_tree)
-        text_encoding = encode_text(text_model, word_idx_map, text)
-        if text_encoding is None and cpc_encoding is None:
-            not_found = not_found + 1
-        else:
-            vec = None
-            if text_encoding is not None and cpc_encoding is not None:
-                vec = text_encoding / np.linalg.norm(text_encoding) + cpc_encoding / np.linalg.norm(cpc_encoding)
-            elif text_encoding is not None:
-                vec = text_encoding
-            else:
-                vec = cpc_encoding
-            vec = vec / np.linalg.norm(vec)
-            #print('ID: ', id)
-            #print('Text: ', text)
-            #print('CPC: ', cpc_tree)
-            #print('Embedding', vec)
-            ingest_cursor.execute(ingest_sql, (id, vec.tolist()))
-        if cnt % 1000 == 999:
-            print("Completed: ", cnt, ' Not found: ', not_found)
-            conn2.commit()
-        cnt = cnt + 1
+        text_batch.append(text)
+        cpc_batch.append(cpc_tree)
+        id_batch.append(id)
+        if len(text_batch) >= batch_size:
+            all_cpc_encoding = encode_cpcs(cpc_model, cpc_idx_map, cpc_batch)
+            all_text_encoding = encode_text(text_model, word_idx_map, text_batch)
+            for i in range(len(cpc_encoding)):
+                text_encoding = all_text_encoding[i]
+                cpc_encoding = all_cpc_encoding[i]
+                vecs = []
+                if text_encoding is None and cpc_encoding is None:
+                    not_found = not_found + 1
+                else:
+                    vec = None
+                    if text_encoding is not None and cpc_encoding is not None:
+                        vec = text_encoding / np.linalg.norm(text_encoding) + cpc_encoding / np.linalg.norm(cpc_encoding)
+                    elif text_encoding is not None:
+                        vec = text_encoding
+                    else:
+                        vec = cpc_encoding
+                    vec = vec / np.linalg.norm(vec)
+                    vecs.append((id, vec.tolist()))
+                if cnt % 1000 == 999:
+                    print("Completed: ", cnt, ' Not found: ', not_found)
+                args_str = ','.join(ingest_cursor.mogrify("(%s,%s)", encoding) for encoding in vecs)
+                sql = ingest_sql_prefix + args_str + ingest_sql_suffix
+                ingest_cursor.execute(sql)
+                conn2.commit()
+                cnt = cnt + 1
+            text_batch.clear()
+            cpc_batch.clear()
+            id_batch.clear()
 
     conn.close()
     conn2.close()
