2a5c291d520b7c427ba40ff44925aedfae117ab9
Evan Hallmark <ehallmark@gttgrp.com>
2018-06-12 12:39:54 -0700

    c

--- src/server/EncodingServer.py
+++ src/server/EncodingServer.py
@@ -1,16 +1,34 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask import request
 from src.models.Word2VecModel import load_cpc_to_index_map
 import psycopg2
 import pandas as pd
+import numpy as np
+from src.java_compatibility.BuildPatentEncodings import load_model, load_word2vec_index_maps, norm_across_rows, encode_text, extract_text_model
 
 app = Flask(__name__)
 
+model_file_128 = '/home/ehallmark/data/python/w2v_cpc128_rnn_model_keras128.h5'
+encoder = load_model(model_file_128)
+model = encoder.model
+model.summary()
+
+text_model = extract_text_model(model)
+word_idx_map = load_word2vec_index_maps()
+
 @app.route("/encode")
 def encode():
-    to_encode = request.args.getlist('data[]', type=str)
-    print(to_encode)
-    return ', '.join(to_encode)
+    to_encode = request.args.get('text', type=str)
+    print('Received text: ', to_encode)
+    encoding = []
+    if to_encode is not None:
+        all_text_encoding = encode_text(text_model, word_idx_map, [to_encode])
+        text_norm = norm_across_rows(all_text_encoding)
+        all_text_encoding = all_text_encoding / np.where(text_norm != 0, text_norm, 1)[:, np.newaxis]
+        if all_text_encoding.shape[0]>0:
+            encoding = all_text_encoding.flatten().tolist()
+    print('Encoding found: ', encoding)
+    return jsonify(encoding)
 
 
 if __name__ == '__main__':
