10fc6f589c984a02979ca81d4f2479d8e7e6e7bc
ehallmark <ehallmark@gttgrp.com>
2018-09-02 12:07:02 -0700

    c

--- src/wipo/Train.py
+++ src/wipo/Train.py
@@ -3,7 +3,7 @@ import pandas as pd
 conn = create_engine("postgresql://localhost/patentdb?user=postgres&password=password")
 from sklearn import preprocessing
 import numpy as np
-from keras.layers import Dense, Reshape, Bidirectional, Embedding, Add, Multiply, Concatenate, Lambda, Input, BatchNormalization, Dropout
+from keras.layers import Dense, Reshape, Bidirectional, Flatten, Embedding, Add, Multiply, Concatenate, Lambda, Input, BatchNormalization, Dropout
 from keras.models import Model
 from keras.optimizers import Adam
 import sklearn.metrics as metrics
@@ -30,10 +30,10 @@ if use_cache:
     data = pd.read_hdf(data_file, 'data')
     cpc_definitions_sql = pd.read_hdf(cpc_definition_file, 'definition')
 else:
-    cpc_definitions_sql = pd.read_sql('select code from big_query_cpc_definition where not code like \'%%/%%\'', conn)
+    cpc_definitions_sql = pd.read_sql('select code from big_query_cpc_definition where char_length(code) < 9 union select \'A\' ', conn)
     cpc_definitions_sql.to_hdf(cpc_definition_file, 'definition', mode='w')
     data = pd.read_sql('select c.publication_number_full, wipo_technology, tree from big_query_wipo_by_pub_flat as w join big_query_cpc_tree as c on (c.publication_number_full=w.publication_number_full)', conn)
-    data['tree'] = [random_select([d for d in dat if not '/' in d]) for dat in data['tree']]
+    data['tree'] = [[d for d in dat if len(d) < 8] for dat in data['tree']]
     data.to_hdf(data_file, 'data', mode='w')
     print('Loading cpc definitions')
 
@@ -66,25 +66,39 @@ for i in range(test_data.shape[0]):
     wipo_encod_test[i, wipo_test[i]] = 1
 
 print('Building cpc dataset')
-cpc = cpc_encoder.transform(data['tree'].iloc[:])
-cpc_test = cpc_encoder.transform(test_data['tree'].iloc[:])
+cpc_encod_test = np.zeros([test_data.shape[0], len(cpc_encoder.classes_)])
+for i in range(test_data.shape[0]):
+    cpc_test = cpc_encoder.transform(test_data['tree'].iloc[i])
+    for j in range(len(cpc_test)):
+        cpc_encod_test[i, cpc_test[j]] = 1
 
-test_data = (cpc_test, wipo_encod_test)
+test_data = (cpc_encod_test, wipo_encod_test)
 
 print('Final wipo shape', wipo_encod.shape)
-print('Final cpc shape', wipo_encod.shape)
+
+
+def generator(batch_size):
+    wipo_encod_sample = np.zeros([batch_size, len(wipo_encoder.classes_)])
+    cpc_encod_sample = np.zeros([batch_size, len(cpc_encoder.classes_)])
+    for i in range(batch_size):
+        r = random.randint(0, data.shape[0])
+        wipo_encod_sample[i] = wipo_encod[r]
+        cpc = cpc_encoder.transform(data['tree'].iloc[r])
+        for j in range(len(cpc)):
+            batch_size[i, cpc[j]] = 1
+    return cpc_encod_sample, wipo_encod_sample
+
 
 model_file = 'wipo_prediction_model.h5'
-hidden_units = 512
-num_layers = 4
-dropout = 0.5
+hidden_units = 1024
+num_layers = 3
+dropout = 0
 batch_size = 256
 
 X = Input((len(cpc_encoder.classes_),))
 
 model = X
-embedding = Embedding(len(cpc_encoder.classes_), hidden_units)
-model = embedding(model)
+
 for i in range(num_layers):
     model = Dense(hidden_units, activation='tanh')(model)
     if dropout > 0:
@@ -93,7 +107,7 @@ for i in range(num_layers):
 model = Dense(len(wipo_encoder.classes_), activation='softmax')(model)
 
 model = Model(inputs=X, outputs=model)
-model.compile(optimizer=Adam(lr=0.0005, decay=0.01), loss='categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer=Adam(lr=0.001, decay=0.1), loss='categorical_crossentropy', metrics=['accuracy'])
 
 model.summary()
 avg_error = test_model(model, test_data[0], test_data[1])
@@ -103,8 +117,9 @@ best_error = avg_error
 errors = []
 errors.append(avg_error)
 for i in range(30):
-    model.fit(cpc, wipo_encod, batch_size=batch_size, initial_epoch=i, epochs=i + 1, validation_data=test_data,
-              shuffle=True)
+    #model.fit(cpc_encod, wipo_encod, batch_size=batch_size, initial_epoch=i, epochs=i + 1, validation_data=test_data,
+    #          shuffle=True)
+    model.fit_generator(generator, steps_per_epoch=data.shape[0]/batch_size, initial_epoch=i, epochs=i + 1, validation_data=test_data)
     avg_error = test_model(model, test_data[0], test_data[1])
     print('Average error: ', avg_error)
     if best_error is None or best_error > avg_error:
